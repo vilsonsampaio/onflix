726fbda9c738ccaccff6d7b362e2ce1d6afd7388
Vilson Sampaio <vilson.sampaio04@gmail.com>
2022-04-15 07:27:14 -0300

    chore: add toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import { ToastContainer } from 'react-toastify';
 
 import { GlobalRoutes } from '@routes/GlobalRoutes';
 
-import GlobalStyles from '@styles/global';
 import defaultTheme from '@styles/themes/default';
+import 'react-toastify/dist/ReactToastify.css';
+import GlobalStyles from '@styles/global';
 
 function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={defaultTheme}>
+        <ToastContainer />
+
         <GlobalStyles /> 
 
         <GlobalRoutes />
