662a647fa794377fb5d6ad442710e32b08e0e46b
Vilson Sampaio <vilson.sampaio04@gmail.com>
2022-04-15 21:00:03 -0300

    feat: create Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,166 @@
-import { Link } from "react-router-dom";
+import { FormEvent, useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import axios from "axios";
 
-import { Container } from "./styles";
+import { Button } from "@components/Button";
+import { Input } from "@components/Input";
+import { Card } from "@components/Card";
+import { Loading } from "@components/Loading";
+
+import MoviesService, { Movie } from "@services/MoviesService";
+
+import { Search } from "@assets/images";
+
+import { Container, OnTrendingContainer, SearchContainer, Title, Wrapper } from "./styles";
+
+type Sort = 'popularity.desc' | 'revenue.desc';
 
 export function Home() {
-  return (
-    <Container>
-      <h1>Home page</h1>
+  const navigate = useNavigate();
 
-      <Link to="/insert_id_here">Go to detail page</Link>
-    </Container>
+  const firstRender = useRef(true);
+  const firstLoading = useRef(true);
+
+  const [selectedSort, setSelectedSort] = useState<Sort>('popularity.desc');
+
+  const [search, setSearch] = useState('');
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [isLoadingMore, setIsLoadingMore] = useState(false);
+  
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [lastPage, setLastPage] = useState(1);
+
+  function handleSearch(e: FormEvent) {
+    e.preventDefault();
+
+    alert(JSON.stringify({ search }));
+  }
+
+  useEffect(() => {
+    async function getMovies(page: number, sortBy: Sort) {
+      try {
+        const { data } = await MoviesService.listMovies({ page, sortBy });
+
+        setMovies((prevState) => {
+          const prevArr = currentPage === 1 ? [] : prevState;
+          
+          return [...prevArr, ...data.results];
+        });
+        setLastPage(data.total_pages);
+      } catch (error) {
+        console.log(error);
+  
+        if (axios.isAxiosError(error)) {
+          if (error.response) {
+            toast.error(error.response.data.status_message || 'Unable to load movies');
+          }
+        }
+      } finally {
+        setIsLoading(false);
+        setIsLoadingMore(false);
+        if (firstLoading.current) firstLoading.current = false;
+      }
+    }
+
+    getMovies(currentPage, selectedSort);
+  }, [currentPage, selectedSort]);
+
+  // Setting page 1 every time the selectedSort changes
+  useEffect(() => {
+    // Blocking below code if is first render
+    if (firstRender.current) {
+      firstRender.current = false;
+      return;
+    }
+
+    setCurrentPage(1);
+    setMovies([]);
+    setIsLoading(true);
+  }, [selectedSort]);
+
+  return (
+    <Wrapper>
+      <Container className="container">
+        <Title>
+          <h1>OnFlix</h1>
+          <p>Hello, welcome! OnFlix is a <span>platform for discovering and storing Movies and TV Shows</span>, rating them as favorites. We are like Goodreads, but focused on television productions.</p>
+        </Title>
+
+        <SearchContainer className="search" onSubmit={handleSearch}>
+          <Input 
+            label="Search for Movies or TV Shows" 
+            leftIcon={<Search />} 
+            value={search}
+            onChange={e => setSearch(e.target.value)}
+          />
+
+          <Button type="submit" disabled={!search}>Search</Button>
+        </SearchContainer>
+
+        <OnTrendingContainer>
+          <nav className="filters">
+            <ul>
+              <li>
+                <button 
+                  className={selectedSort === 'popularity.desc' ? 'selected' : ''}
+                  onClick={() => setSelectedSort('popularity.desc')}
+                >
+                  Popularity
+                </button>
+
+                <button 
+                  className={selectedSort === 'revenue.desc' ? 'selected' : ''}
+                  onClick={() => setSelectedSort('revenue.desc')}
+                >
+                  Revenue
+                </button>
+              </li>
+            </ul>
+          </nav>
+
+          <h2>
+            Most {selectedSort === 'popularity.desc' ? 'Popular' : 'Rent'} Movies
+            <span>({isLoading || isLoadingMore ? 0 : movies.length})</span>
+          </h2>
+
+          <div className="results-container">
+            {isLoading
+              ? <Loading /> 
+              : movies.length > 0 
+                ? movies.map((movie, i) => (
+                    <Card 
+                      key={`${movie.id}--${i}`}
+                      data={{
+                        id: movie.id,
+                        title: movie.title,
+                        posterPath: movie.poster_path,
+                        rating: movie.vote_average,
+                        isFavorited: (i + 1) % 2 === 0
+                      }} 
+                      onClick={() => navigate(`/movie/${movie.id}`)}
+                    />
+                ))
+                : 'No movies...'
+            }
+          </div>
+         
+          {!isLoading && (
+            <Button 
+              disabled={currentPage >= lastPage}
+              isLoading={isLoadingMore}
+              onClick={() => {
+                setCurrentPage(prevState => prevState + 1);
+                setIsLoadingMore(true);
+              }}
+            >
+              Load more
+            </Button>
+          )}
+        </OnTrendingContainer>
+      </Container>
+    </Wrapper>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,6 +1,131 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+import { Wrapper as Input } from '@components/Input/styles';
+import { Wrapper as Button } from '@components/Button/styles';
+
+import { convertToRem } from '@utils/convertToRem';
+
+export const Wrapper = styled.div`
+  width: 100%;
+`;
 
 export const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
+  padding: ${convertToRem(80)} 0;
+`;
+
+export const Title = styled.div`
+  ${({ theme }) => css`
+    max-width: ${convertToRem(590)};
+
+    h1 {
+      color: ${theme.colors.neutrals.grey.Grey50};
+
+      ${theme.fonts.heading.one};
+    }
+
+    p {
+      margin-top: ${convertToRem(16)};
+
+      color: ${theme.colors.neutrals.grey.Grey300};
+
+      ${theme.fonts.body.regular};
+
+      span {
+        font-weight: 600;
+        color: ${theme.colors.primary.Primary300};
+      }
+    }
+  `};
+`;
+
+export const SearchContainer = styled.form`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+
+  max-width: ${convertToRem(600)};
+
+  margin-top: ${convertToRem(24)};
+
+  ${Input} {
+    flex: 1;
+  }
+
+  ${Button} {
+    align-self: stretch;
+    
+    margin-left: ${convertToRem(16)};
+  }
+`;
+
+export const OnTrendingContainer = styled.section`
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+
+    margin-top: ${convertToRem(80)};
+
+    .filters {
+      max-width: 100%;
+
+      ul {
+        display: flex;
+        flex-wrap: wrap;
+
+        padding: ${convertToRem(8)};
+
+        background-color: ${theme.colors.neutrals.black.Black20};
+
+        border-radius: ${convertToRem(12)};
+      }
+
+      button {
+        padding: ${convertToRem(8)} ${convertToRem(32)} ;
+
+        background-color: transparent;
+        color: ${theme.colors.neutrals.grey.Grey300};
+
+        ${theme.fonts.link.regular};
+
+        border: 0;
+        border-radius: ${convertToRem(8)};
+
+        &.selected {
+          background-color: ${theme.colors.primary.Primary400};
+          color: ${theme.colors.primary.Primary50};
+        }
+      }
+    }
+
+    h2 {
+      margin-top: ${convertToRem(24)};
+
+      color: ${theme.colors.neutrals.grey.Grey400};
+
+      ${theme.fonts.heading.three};
+
+      span {
+        margin-left: ${convertToRem(8)};
+
+        ${theme.fonts.body.regular};
+      }
+    }
+
+    .results-container {
+      display: grid;
+      grid-template-columns: repeat(4, 1fr);
+      gap: ${convertToRem(24)};
+
+      width: 100%;
+
+      margin-top: ${convertToRem(24)};
+    }
+
+    > ${Button} {
+      margin: 0 auto;
+      margin-top: ${convertToRem(48)};
+    }
+  `}
 `;
